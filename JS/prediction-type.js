c6ebc0c102aa6c9171935f6292c6fbf867ca08a6
elina2708 <elina.maugis@isen-ouest.yncrea.fr>
2025-06-18 14:25:07 +0200

    Update prediction-type.js

--- JS/prediction-type.js
+++ JS/prediction-type.js
@@ -1,1 +1,12 @@
-
+fetch('php/prediction_type.php')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('nom').textContent = data.VesselName;
+    document.getElementById('longueur').textContent = data.Length + ' m';
+    document.getElementById('largeur').textContent = data.Width + ' m';
+    document.getElementById('tirant').textContent = data.Draft + ' m';
+    document.getElementById('type').textContent = data.prediction;
+  })
+  .catch(error => {
+    console.error('Erreur lors de la récupération des données :', error);
+  });
